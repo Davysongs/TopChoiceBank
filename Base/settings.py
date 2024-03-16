c0c094825f0480a026c5bbc1ba66a1bbb02dd978
Davysongs <Davidgsongs@gmail.com>
2024-03-16 10:11:54 +0100

    Deployment

--- Base/settings.py
+++ Base/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pa*7qjsodoj62^x^a@n9v!c)91+ta+9!t31d667_wzc%z*t#1#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
