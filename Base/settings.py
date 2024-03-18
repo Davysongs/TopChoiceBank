aaab061084975fb70bfe49768dfdf95e56ae757c
Davysongs <Davidgsongs@gmail.com>
2024-03-18 15:57:21 +0100

    Dev init

--- Base/settings.py
+++ Base/settings.py
@@ -75,27 +75,28 @@ WSGI_APPLICATION = 'Base.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'topchoice',
-#         'USER': 'root',
-#         'PASSWORD':'#$(tVA0nr)',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306'
-#     }
-# }
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'railway',
-       'USER':'postgres',
-       'PASSWORD': 'gzwqUNzVkcgbSWMfmhkBKhHvOTtlXBCm',
-       'HOST':'roundhouse.proxy.rlwy.net',
-       'PORT':'14415',
-
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'topchoice',
+        'USER': 'root',
+        'PASSWORD':'#$(tVA0nr)',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
+    }
 }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'railway',
+#        'USER':'postgres',
+#        'PASSWORD': 'gzwqUNzVkcgbSWMfmhkBKhHvOTtlXBCm',
+#        'HOST':'roundhouse.proxy.rlwy.net',
+#        'PORT':'14415',
+
+#    }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
