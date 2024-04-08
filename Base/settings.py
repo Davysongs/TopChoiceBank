81040498b593f8f6b6c8d018bfaa165d6476ab94
Davysongs <Davidgsongs@gmail.com>
2024-04-08 18:31:25 +0100

    environment

--- Base/settings.py
+++ Base/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pa*7qjsodoj62^x^a@n9v!c)91+ta+9!t31d667_wzc%z*t#1#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,12 +88,12 @@ WSGI_APPLICATION = 'Base.wsgi.application'
 
 DATABASES = {
    'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'railway',
-       'USER':'postgres',
-       'PASSWORD': 'gzwqUNzVkcgbSWMfmhkBKhHvOTtlXBCm',
-       'HOST':'roundhouse.proxy.rlwy.net',
-       'PORT':'14415',
+       'ENGINE': os.getenv('DATABASE_ENGINE'),
+       'NAME': os.getenv('DATABASE_NAME'),
+       'USER':os.getenv('DATABASE_USER'),
+       'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+       'HOST':os.getenv('DATABASE_HOST'),
+       'PORT':os.getenv('DATABASE_PORT'),
 
    }
 }
@@ -160,12 +160,12 @@ LOGOUT_URL = "logout"
 
 #email stuff
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_HOST')
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'koladegeorge5@gmail.com'
-EMAIL_HOST_PASSWORD = 'lqvvdmxcswrmovvr'
-DEFAULT_FROM_EMAIL = 'Top-Choice-Bank <no-reply@topchoicebank.com>'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 
 
